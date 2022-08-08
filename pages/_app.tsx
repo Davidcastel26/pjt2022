f7bc0e73423d04e285193424844cbd83e7250c98
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-08-07 18:20:28 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
+import {useState, useEffect} from 'react'
 import type { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+
+  const [isSSR, setIsSSR] = useState(true)
+
+  useEffect( () => {
+    setIsSSR(false)
+  },[])
+
+  if(isSSR) return null;
+
+  return(
+    <div>
+      Navbar
+      {/* flex gap-6 md:gap-20 */}
+      <div>
+        {/* h-92vh overflox-hidden xl hover:overflow-auto */}
+        <div>
+          Sidebar 
+        </div>  
+      </div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
