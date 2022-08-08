09816ffd3793e83ee140df83e4ac77671291f7fb
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-08-07 18:45:02 -0600

    setinng styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import {useState, useEffect} from 'react'
 import type { AppProps } from 'next/app'
 
+import Navbar from '../components/Navbar'
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
 
   const [isSSR, setIsSSR] = useState(true)
@@ -14,7 +16,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return(
     <div>
-      Navbar
+      <Navbar />
       {/* flex gap-6 md:gap-20 */}
       <div>
         {/* h-92vh overflox-hidden xl hover:overflow-auto */}
